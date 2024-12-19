8dd9359cbd7ffa5d9c79b2eac4bb52404948495c
tamizhselvangt <tamizhselvangt@gmail.com>
2024-12-19 10:14:34 +0530

    Shared Preferences is added

--- lib/models/flash_card_model.dart
+++ lib/models/flash_card_model.dart
@@ -1,19 +1,28 @@
 
 
-import 'dart:convert';
 
-
-@JsonEncoder()
-@JsonEncoder()
 class Flashcard {
   String id;
   String question;
   String answer;
 
-  Flashcard ({
+  Flashcard({
     required this.id,
     required this.question,
     required this.answer,
   });
 
+  // Convert a Flashcard into a Map
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'question': question,
+    'answer': answer,
+  };
+
+  // Create a Flashcard from a Map
+  factory Flashcard.fromJson(Map<String, dynamic> json) => Flashcard(
+    id: json['id'],
+    question: json['question'],
+    answer: json['answer'],
+  );
 }
